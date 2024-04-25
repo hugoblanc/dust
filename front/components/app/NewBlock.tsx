12751cb05e3d63d5447a0c25a00aab5442224249
Alban Dumouilla <alban@dust.tt>
2024-04-25 11:38:48 +0200

    Getting dataset data from GET

--- front/components/app/NewBlock.tsx
+++ front/components/app/NewBlock.tsx
@@ -27,12 +27,6 @@ export default function NewBlock({
     name: string;
     description: string;
   }[] = [
-    // {
-    //   type: "llm",
-    //   typeNames: ["llm"],
-    //   name: "Large Language Model (LLM)",
-    //   description: "Query a Large Language Model to complete a prompt for you.",
-    // },
     {
       type: "chat",
       typeNames: ["chat"],
