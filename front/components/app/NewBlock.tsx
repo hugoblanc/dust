fb7918202135cff7741ab16a9c6609ed31f2a350
Alban Dumouilla <alban@dust.tt>
2024-04-29 10:30:30 +0200

    Put back LLM block

--- front/components/app/NewBlock.tsx
+++ front/components/app/NewBlock.tsx
@@ -28,9 +28,15 @@ export default function NewBlock({
     description: string;
   }[] = [
     {
+      type: "llm",
+      typeNames: ["llm"],
+      name: "Large Language Model (LLM)",
+      description: "Query a Large Language Model to complete a prompt for you.",
+    },
+    {
       type: "chat",
       typeNames: ["chat"],
-      name: "Large Language Model",
+      name: "Chat-based Large Language Model",
       description:
         "Query a Large Language Model using a message-based interface.",
     },
